0cdaf9b36c77511200491c07f73544b3343e4ada
Alyssa X <hi@alyssax.com>
2021-01-09 22:56:35 +0100

    Camera source fix (when recording)

--- js/camera.js
+++ js/camera.js
@@ -8,6 +8,7 @@ var output = new MediaStream();
 var audioCtx;
 var destination;
 var micsource;
+var constraints
 var cancel = false;
 var recording = false;
 var newwindow = null;
@@ -17,13 +18,6 @@ var htmlinject = "<video id='injected-video' style='height:100%;position:absolut
 document.body.innerHTML += htmlinject;
 var video = document.getElementById("injected-video");
 
-// Video dimensions and settings
-var constraints = {
-  audio: false,
-  video: {width:1920, height:1080}
-};
-
-navigator.mediaDevices.getUserMedia(constraints).then(startStreaming).catch(streamingError);
 function startStreaming(stream) {
     camerastream = stream;
     
@@ -146,7 +140,17 @@ chrome.storage.sync.get(['flip'], function(result) {
 chrome.storage.sync.get(['camera'], function(result) {
     if (result.camera != 0) {
         updateCamera(result.camera);
+        constraints = {
+          audio: false,
+          video: {deviceId:result.camera, width:1920, height:1080}
+        };
+    } else {
+        constraints = {
+          audio: false,
+          video: {deviceId:result.camera, width:1920, height:1080}
+        };
     }
+    navigator.mediaDevices.getUserMedia(constraints).then(startStreaming).catch(streamingError);
 });
 
 // Listen for messages
