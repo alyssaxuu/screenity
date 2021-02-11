84ba00bce03a6842f22811a51b02d04edf9cb61e
Prashant Shahi <coolboi567@gmail.com>
2021-02-11 13:39:07 +0530

    append iframe only if body is defined

--- js/detect.js
+++ js/detect.js
@@ -3,10 +3,10 @@ var iframe = document.createElement('iframe');
 iframe.style.display = "none";
 iframe.src = chrome.extension.getURL('./html/sources.html');
 iframe.allow = "camera";
-document.body.appendChild(iframe);
+document.body && document.body.appendChild(iframe);
 
 var iframe = document.createElement('iframe');
 iframe.style.display = "none";
 iframe.src = chrome.extension.getURL('./html/audiosources.html');
 iframe.allow = "microphone";
-document.body.appendChild(iframe);
+document.body && document.body.appendChild(iframe);
