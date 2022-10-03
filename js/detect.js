2822804a160591be0ef66d576d161bb68dcb45be
Cho Yin Yong <choyiny@gmail.com>
2022-10-02 20:16:49 -0400

    chore: run prettier

--- js/detect.js
+++ js/detect.js
@@ -1,12 +1,12 @@
 // Inject the iframe to retrieve camera sources
-var iframe = document.createElement('iframe');
+var iframe = document.createElement("iframe");
 iframe.style.display = "none";
-iframe.src = chrome.extension.getURL('./html/sources.html');
+iframe.src = chrome.extension.getURL("./html/sources.html");
 iframe.allow = "camera";
 document.body.appendChild(iframe);
 
-var iframe = document.createElement('iframe');
+var iframe = document.createElement("iframe");
 iframe.style.display = "none";
-iframe.src = chrome.extension.getURL('./html/audiosources.html');
+iframe.src = chrome.extension.getURL("./html/audiosources.html");
 iframe.allow = "microphone";
 document.body.appendChild(iframe);
