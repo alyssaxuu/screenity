6730109a7e1d483646ca45fd7c924732a717cfe6
Alyssa X <hi@alyssax.com>
2024-03-02 19:55:59 +0000

    Editor page improvements and duration fix

--- src/pages/EditorFallback/utils/fetchFile.js
+++
@@ -1,6 +0,0 @@
-async function fetchFile(url) {
-  const response = await fetch(url);
-  return new Uint8Array(await response.arrayBuffer());
-}
-
-export default fetchFile;
--- utils/env.js
+++
@@ -1,5 +0,0 @@
-// tiny wrapper with default env vars
-module.exports = {
-  NODE_ENV: process.env.NODE_ENV || 'development',
-  PORT: process.env.PORT || 3000,
-};
